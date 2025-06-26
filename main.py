6ac70ca3d3e380f5473da11ce86f8008d825f96e
mertsariyar <mertsariyar60@gmail.com>
2025-06-27 02:32:02 +0300

    added shoting functionality by creating shot.py and update it on player.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from circleshape import CircleShape
+from shot import Shot
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -13,6 +14,7 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     ###
     Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
@@ -20,7 +22,8 @@ def main():
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable,)
     asteroid_field = AsteroidField()
-
+    ###
+    Shot.containers = (shots, updatable, drawable)
     dt = 0
     while True:
         for event in pygame.event.get():
